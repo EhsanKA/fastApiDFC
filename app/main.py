6c21b7cc643dcb496897fb53ecd032525660f4a9
EhsanKA <e.karimiara@gmail.com>
2024-01-25 16:02:46 +0100

    major changes on time related functions

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from app.schemas import OrderRequest
 from app.fee_calculator import FeeCalculator
 
@@ -8,6 +8,6 @@ app = FastAPI()
 @app.post("/orders/")
 def create_order(order_req: OrderRequest):
     fee_calculator = FeeCalculator(order_req.cart_value, order_req.delivery_distance,
-                                   order_req.number_of_items, order_req.order_time)
+                                   order_req.number_of_items, order_req.order_time_str)
     delivery_fee = fee_calculator.calculate_delivery_fee()
     return {"delivery_fee": delivery_fee}
